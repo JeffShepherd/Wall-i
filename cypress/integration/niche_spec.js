25928626bec770e70c198ddd51307a6bc91b0f42
JS <jeff.shep777@gmail.com>
2021-04-25 18:47:28 -0600

    Add homepage tests

--- cypress/integration/niche_spec.js
+++ cypress/integration/niche_spec.js
@@ -15,11 +15,14 @@ describe('Home page', () => {
     cy.visit('http://localhost:3000/')
   })
 
-  it('Show a hero photo on load', () => {
-    cy.get('img[id=uYA3q_83I5U]').should('be.visible')
+  it('Should show a hero photo on load', () => {
+    cy.get('img[id=uYA3q_83I5U]')
+      .should('be.visible')
+      .should('have.attr', 'src')
+      .should('include', 'https://images.unsplash.com/photo-1616952801670-d33ab59d45bc?crop=entropy&cs=srgb&fm=jpg&ixid=MnwyMjQ5MjF8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MTkzODc3NjE&ixlib=rb-1.2.1&q=85')
   })
 
-  it('Shows have a favorites link', () => {
+  it('Should have a favorites link', () => {
     cy.get('p').contains('favorites')
   })
 
@@ -27,4 +30,29 @@ describe('Home page', () => {
     cy.get('form').children().should('have.length', 2)
   })
 
+  it('Should update the URL when clicking favorites', () => {
+    cy.get('.favorite-link').click()
+      .url().should('eq', 'http://localhost:3000/favorites')
+  })
+
 })
+
+
+// describe('Sad paths', () => {
+
+//   beforeEach(() => {
+//     cy.fixture('random.json')
+//     .then(randomData => {
+//       cy.intercept(
+//         'GET',
+//         `https://api.unsplash.com/photos/random/?client_id=${Cypress.env('KEY')}&orientation=squarish`,
+//         { 
+//           statusCode: 200,
+//           body: randomData
+//         }
+//       )
+//     })
+//     cy.visit('http://localhost:3000/')
+//   })
+
+// })
